7cdb40e3d03a3c0434b9091a6953d74f461a2d4a
Mel/ <tchoikuemel06@gmail.com>
2024-04-08 01:17:18 +0100

    add youtube font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-export const API_KEY='AIzaSyDVXBKOWzHEP1YGPPTpc66NibPvy26C4Vg';
+export const API_KEY = 'AIzaSyDVXBKOWzHEP1YGPPTpc66NibPvy26C4Vg';
 
 export const valueConverter = (value) => {
 	if (value >= 1000000) {
-		return Math.floor(value / 1000000) + 'M';
+		return Math.floor(value / 1000000) + ' ' + 'M';
 	} else if (value >= 1000) {
-		return Math.floor(value / 1000) + 'K';
+		return Math.floor(value / 1000) + ' ' + 'K';
 	} else {
 		return value;
 	}
